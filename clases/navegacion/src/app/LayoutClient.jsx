3057e0729a88af52c2fe6f8bea9a729dc8e14435
clavijo99 <bclavijogomez@gmail.com>
2025-08-27 10:38:41 -0500

    feat: implement LayoutClient component with Header and Footer imports

---
+++ clases/navegacion/src/app/LayoutClient.jsx
@@ -0,0 +1,13 @@
+
+import {
+    Header,
+    Footer
+} from '@/components/index'
+
+export default function LayoutClient({ childern }){
+    return (
+        <div>
+
+        </div>
+    )
+}
---
+++ clases/navegacion/src/components/index.jsx
@@ -0,0 +1,10 @@
+import Header from "./ui/Header/Header";
+import Footer from "./ui/Footer/Footer";
+
+
+
+
+export {
+    Header,
+    Footer
+}
