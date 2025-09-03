6645af234594373a8d7fe820dbfc624fbdebd5a8
clavijo99 <bclavijogomez@gmail.com>
2025-09-03 08:51:49 -0500

    feat: enhance LayoutClient component with conditional Header and Footer rendering

--- clases/navegacion/src/app/LayoutClient.jsx
+++ clases/navegacion/src/app/LayoutClient.jsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 
 import {
     Header,
@@ -5,12 +8,32 @@ import {
 } from '@/components/index'
 
 
+// paginas que no quiero mostrar el header y footer
+const hiddenPaths = ['/loging', '/register'];
 
 
-export default function LayoutClient({ childern }){
+export default function LayoutClient({ childern }) {
+
+    // creamos una constante para manejar el path
+    const pathName = usePathname();
+
+    const hidden = hiddenPaths.includes(pathName);
+
     return (
-        <div>
-            
+        <div style={{
+            minHeight: "100vh",
+            display: "flex",
+            flexDirection: "column"
+        }}>
+
+            {!hidden && <Header />}
+
+            <main style={{ flex: 1 }}>
+                {childern}
+            </main>
+
+            {!hidden && <Footer />}
+
         </div>
     )
 }
