0771859cce5a5ebb855eceb36bd621fed0b0d90e
clavijo99 <bclavijogomez@gmail.com>
2025-09-17 09:39:59 -0500

    feat: refactor Home component to manage state for incrementing and decrementing a number; update ButtonCustom click handlers

--- clases/estados-locales/src/app/page.jsx
+++ clases/estados-locales/src/app/page.jsx
@@ -1,17 +1,23 @@
 'use client'
 
 import { useState } from 'react'
-
 import ButtonCustom from "@/components/common/ButtonCustom";
 
-
 export default function Home() {
-
   const [nombre, setNombre] = useState('Camilo')
+  const [numero, setNumero ] = useState(0)
 
+  function incrementar(){
+    setNumero((n) => n + 1)
+  }
 
-  var numero = 0
-
+  function decrementar(){
+    if( numero > 0 ){
+      setNumero((n) => n - 1)
+    }else{
+      alert('El numero no puede ser negativo')
+    }
+  }
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
@@ -20,11 +26,12 @@ export default function Home() {
 
       <div>
         <div className='flex'>
-          <ButtonCustom texto="+" />
+          <ButtonCustom texto="+" click={incrementar} />
           <p className='px-4' >{numero}</p>
-          <ButtonCustom texto="-" />
-
+          <ButtonCustom texto="-" click={decrementar} />
         </div>
+
+        <p className='py-6'>El numero {numero} es { numero %2== 0 ? 'Par': 'Impar' }</p>
       </div>
     </div>
   );
